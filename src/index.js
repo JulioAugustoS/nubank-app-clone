9f7efbf52d2753fa317ae792718e965b022edcef
Julio Augusto <julio@lapse.com.br>
2019-05-01 01:12:40 -0300

    config icons and splash screen in project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { StatusBar } from 'react-native';
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
-const App = () => (
-  <>
-    <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
-    <Routes />
-  </>
-);
+function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
+      <Routes />
+    </>
+  );
+}
 
 export default App;
